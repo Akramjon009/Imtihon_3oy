a067f60dbbd57961b82b95ae551ce2ffd8a4b149
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-02 11:34:59 +0500

    add user model

--- OrderManagementAPI.Dome/Entites/Models/UserModel.cs
+++ OrderManagementAPI.Dome/Entites/Models/UserModel.cs
@@ -2,7 +2,11 @@
 {
     public class UserModel
     {
+        public long Id { get; set; }
         public string FullName { get; set; }
-    
+        public string Email { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+        public string Role {  get; set; }
     }
 }
