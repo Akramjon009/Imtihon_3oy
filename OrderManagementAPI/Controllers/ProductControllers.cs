841dd35e363459fea2672e6e18b36105fc367e09
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-03-03 17:53:03 +0500

    add permission for Product

--- OrderManagementAPI/Controllers/ProductControllers.cs
+++ OrderManagementAPI/Controllers/ProductControllers.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OrderManagementAPI.Application.Abstractions.IService;
+using OrderManagementAPI.Attrebutes;
 using OrderManagementAPI.Domen.Entites.DTOs;
+using OrderManagementAPI.Domen.Entites.Enums;
 using OrderManagementAPI.Domen.Entites.Models;
 
 namespace OrderManagementAPI.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
+    [Authorize]
     public class ProductControllers : ControllerBase
     {
         private readonly IProductService _productService;
@@ -16,42 +20,49 @@ namespace OrderManagementAPI.Controllers
             _productService = productService;
         }
         [HttpPost]
+        [IdentityFilter(Permission.Create)]
         public async Task<ActionResult<ProductModel>> CreateProduct(ProductDTO product)
         {
             var result = await _productService.Create(product);
             return Ok(result);
         }
         [HttpGet]
+        [IdentityFilter(Permission.GetAll)]
         public async Task<ActionResult<IEnumerable<ProductModel>>> GetAll()
         {
             var result = await _productService.GetAll();
             return Ok(result);
         }
         [HttpGet]
+        [IdentityFilter(Permission.GetById)]
         public async Task<ActionResult<ProductModel>> GetProductById(long id)
         {
             var result = await _productService.GetById(id);
             return Ok(result);
         }
         [HttpPut]
+        [IdentityFilter(Permission.Update)]
         public async Task<ActionResult<ProductModel>> UpdateAllUser(long id, ProductDTO product)
         {
             var result = await _productService.Update(id, product);
             return Ok(result);
         }
         [HttpPatch]
+        [IdentityFilter(Permission.UpdateName)]
         public async Task<ActionResult<ProductModel>> UpdateProductName(long id, string productName)
         {
             var result = await _productService.UpdateName(id, productName);
             return Ok(result);
         }
         [HttpPatch]
+        [IdentityFilter(Permission.UpdateDescription)]
         public async Task<ActionResult<ProductModel>> UpdateProducDesctiption(long id, string productDescription)
         {
             var result = await _productService.UpdateDescription(id, productDescription);
             return Ok(result);
         }
         [HttpPatch]
+        [IdentityFilter(Permission.UpdateCaunt)]
         public async Task<ActionResult<ProductModel>> UpdateProducCaunt(long id, long caunt)
         {
             var result = await _productService.UpdateCaunt(id, caunt);
